905fb0f7464b477312aa6c4d48808a848aa412f9
wilricknl <83393813+wilricknl@users.noreply.github.com>
2024-04-09 10:52:20 +0200

    Lecture 3 Windows conversions

--- lecture03/conversions.cpp
+++ lecture03/conversions.cpp
@@ -7,8 +7,64 @@
 
 int main()
 {
-    // todo: implement
-    std::cout << "Hello, Windows" << std::endl;
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    // short and long conversions
+    unsigned short host_port = 0x1234;
+    unsigned short network_port = 0x0;
+    unsigned long host_address = 0x01020304;
+    unsigned long network_address = 0x0;
+
+    network_port = htons(host_port);
+    network_address = htonl(host_address);
+
+    printf("Host ordered port: %#06x \n", host_port);
+    printf("Network ordered port: %#06x \n", network_port);
+    printf("Host ordered address: %#010lx \n", host_address);
+    printf("Network ordered address: %#010lx \n", network_address);
+
+    // network to address with inet
+    {
+        sockaddr_in address;
+        address.sin_addr.s_addr = network_address;
+        char* network_address_text = inet_ntoa(address.sin_addr);
+        if (network_address_text)
+        {
+            std::cout << "Network address: " << network_address_text << std::endl;
+        }
+    }
+
+    // address to network with inet
+    {
+        if (auto address = inet_addr("5.6.7.8"))
+        {
+            printf("Network ordered address: %#10lx \n", address);
+        }
+    }
+
+    // network to address with Windows
+    {
+        char* string_address = "01.02.03.04:80";
+
+        sockaddr_in address;
+        int address_size = sizeof(address);
+        WSAStringToAddress(string_address, AF_INET, NULL, (SOCKADDR*)&address, &address_size);
+
+        printf("WSAStringToAddress: %#010lx:%#6x\n", address.sin_addr, address.sin_port);
+
+        // address to network with Windows
+        // for some reason it only works after initialization with WSAStringToAddress,
+        // so I guess just use the inet-functions.
+        char buffer[50]{0};
+        DWORD buffer_size = sizeof(buffer);
+        WSAAddressToString((sockaddr*)&address, address_size, NULL, buffer, &buffer_size);
+
+        printf("WSAAddressToString: %s\n", buffer);
+    }
+
+    WSACleanup();
+    return 0;
 }
 
 #else
